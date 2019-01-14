e4bc2d578edb3d533fdc8477109f56b694b326d6
Mark Learst <learst@me.com>
2019-01-14 00:53:44 -0500

    Added Auth0 epic task.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
+import Profile from './Profile'
+import Nav from './Nav'
+import Auth from './Auth/Auth'
+import Callback from './Callback'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.auth = new Auth(this.props.history)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+        <Nav auth={this.auth} />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
+    )
   }
 }
 
-export default App;
+export default App
